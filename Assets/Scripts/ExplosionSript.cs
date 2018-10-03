a3bf213b13a9366e3e0955d3c67027bab5e9d685
ped4735 <ped4735@gmail.com>
2018-10-03 20:07:58 -0300

    Commit Final

--- Assets/Scripts/ExplosionSript.cs
+++ Assets/Scripts/ExplosionSript.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 
 public class ExplosionSript : MonoBehaviour {
 
 	public GameObject exp;
+	
 
 
 	public void StartExplosion(){
 		GameObject obj = Instantiate(exp,this.transform.position,Quaternion.identity);
 		Destroy(gameObject);
+		GameController.instance.EnemyDead();
+
 	}
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class GameController : MonoBehaviour
+    {
+
+
+        public static GameController instance; 
+        
+        public int enemyCount;
+
+        public GameObject panel;
+
+        private void Start()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            
+            enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+            
+            
+            
+        }
+        
+        public void EnemyDead()
+        {
+            enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+            if (enemyCount == 1)
+            {
+                //fim
+                Debug.Log("FIM!");
+                panel.SetActive((true));
+            }
+        }
+
+
+        public void ResetOnClick()
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+        
+        
+        
+    }
+}
