39acb8cda712c71e87cc8a791a7c64e2543eca4a
ppedra <asb@asd.com>
2018-10-02 21:24:12 -0300

    explosão

---
+++ Assets/Scripts/ExplosionDestroyScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionDestroyScript : MonoBehaviour {
+
+	public void EndAnimation(){
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/ExplosionSript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionSript : MonoBehaviour {
+
+	public GameObject exp;
+
+
+	public void StartExplosion(){
+		GameObject obj = Instantiate(exp,this.transform.position,Quaternion.identity);
+	}
+}
